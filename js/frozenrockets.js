f2ea57807e8a1cb30d22a4657c21213c1e244810
Peter van Grieken <hi@frozenrockets.nl>
2014-03-03 10:04:47 +0100

    Cleaned up JS files

--- js/frozenrockets.js
+++ js/frozenrockets.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('devicelight', function(e) {
   var lux = e.value;
 
@@ -10,27 +9,5 @@ window.addEventListener('devicelight', function(e) {
   }
   if(lux > 1000)  { // bright
     document.body.className = 'bright';
-  } 
+  }
 });
-
-
-
-///if (window.DeviceMotionEvent!=undefined) {
-
-        var angle = 0;
-        var multiplier = 9 / 90;
-        window.ondeviceorientation = function(event) {
-            var new_angle = ((self.orientation === "portrait" ) ? event.beta : event.gamma) * multiplier;
-            if(new_angle != angle){
-              $('#angle').text(new_angle);
-                angle = new_angle;
-                $('.home-content .home-background').css({
-                    transform: 'translate3d(0, '+angle+'% , 0)'
-                })
-                $('.home-content .home-link-container .js-home-link .buttonlink').css({
-                    transform: 'translate3d(0, '+(angle*-1)+'px , 0)'
-                });
-            }
-        }
-
-//}
